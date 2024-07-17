568039a7baf8df494372318f3e2cf4d4835ede41
fadi94800 <fadi94800@gmail.com>
2024-07-17 07:02:15 +0200

    ajout photo

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,73 +1,56 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Fonction asynchrone pour récupérer les données des appartements
     async function fetchAppartements() {
         try {
-            // Effectue une requête GET vers l'API pour obtenir les données
             const response = await fetch('http://localhost:5678/api/works');
             if (!response.ok) {
-                // Lance une erreur en cas de réponse non correcte
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
-            // Convertit la réponse en JSON
             const data = await response.json();
-            console.log('Données récupérées:', data); // Affiche les données dans la console pour vérification
-            // Appelle la fonction pour afficher les appartements
             displayAppartements(data);
-            // Appelle la fonction pour afficher toutes les photos dans la modale
             displayAllPhotosInModal(data);
         } catch (error) {
-            // Gère les erreurs éventuelles lors de la requête
             console.error('Erreur lors de la récupération des données:', error);
         }
     }
 
-    // Fonction pour afficher les appartements
     function displayAppartements(appartements) {
-        // Sélectionne l'élément avec l'id listeFilm
         const galerie = document.getElementById('listeFilm');
         if (!galerie) {
-            // Affiche un message d'erreur si l'élément n'est pas trouvé
             console.error('Élément galerie non trouvé');
-            return; // Sort de la fonction
+            return;
         }
-        // Supprime le contenu HTML existant dans l'élément galerie
         galerie.innerHTML = '';
 
-        // Parcourt les données des appartements
         appartements.forEach(appartement => {
-            // Crée un nouvel élément figure pour chaque appartement
             const div = document.createElement('figure');
-            div.className = 'appartement'; // Assigne une classe à l'élément figure pour le CSS
-            // Utilise la propriété category.name pour les catégories et les met en minuscules
+            div.className = 'appartement';
+            div.dataset.id = appartement.id;
             div.dataset.category = appartement.category.name.toLowerCase(); 
 
-            // Crée un nouvel élément figcaption pour le titre
             const title = document.createElement('figcaption');
-            title.textContent = appartement.title; // Définit le texte du titre avec le titre de l'appartement
+            title.textContent = appartement.title;
 
-            // Crée un nouvel élément img pour l'image
             const img = document.createElement('img');
-            img.src = appartement.imageUrl; // Définit la source de l'image avec l'URL de l'image de l'appartement
-            img.alt = appartement.title; // Définit le texte alternatif de l'image avec le titre de l'appartement
+            img.src = appartement.imageUrl;
+            img.alt = appartement.title;
 
-            // Crée un nouvel élément pour l'icône de suppression
             const deleteIcon = document.createElement('div');
             deleteIcon.className = 'delete-icon';
-      
-            deleteIcon.addEventListener('click', () => {
-                deleteAppartement(appartement.id);
+            deleteIcon.innerHTML = '<i class="fa-regular fa-trash-can"></i>';
+
+            deleteIcon.addEventListener('click', (event) => {
+                event.stopPropagation();
+                console.log(`Delete icon clicked for ID: ${appartement.id}`);
+                deleteAppartement(appartement.id, div);
             });
 
-            // Ajoute l'image, le titre et l'icône de suppression à l'élément figure
             div.appendChild(img);
             div.appendChild(title);
             div.appendChild(deleteIcon);
-            // Ajoute l'élément figure à la galerie
             galerie.appendChild(div);
         });
     }
 
-    // Fonction pour afficher toutes les photos dans la modale
     function displayAllPhotosInModal(appartements) {
         const galleryContent = document.querySelector('.gallery-content');
         if (!galleryContent) {
@@ -79,17 +62,20 @@ document.addEventListener('DOMContentLoaded', () => {
         appartements.forEach(appartement => {
             const div = document.createElement('figure');
             div.className = 'appartement-modal';
+            div.dataset.id = appartement.id;
 
             const img = document.createElement('img');
             img.src = appartement.imageUrl;
             img.alt = appartement.title;
 
-            // Crée un nouvel élément pour l'icône de suppression
             const deleteIcon = document.createElement('div');
             deleteIcon.className = 'delete-icon';
             deleteIcon.innerHTML = '<i class="fa-regular fa-trash-can"></i>';
-            deleteIcon.addEventListener('click', () => {
-                deleteAppartement(appartement.id);
+
+            deleteIcon.addEventListener('click', (event) => {
+                event.stopPropagation();
+                console.log(`Delete icon clicked for ID: ${appartement.id}`);
+                deleteAppartement(appartement.id, div);
             });
 
             div.appendChild(img);
@@ -98,52 +84,161 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    // Fonction pour filtrer les appartements en fonction de la catégorie
+    async function deleteAppartement(id, element) {
+        console.log(`Attempting to delete ID: ${id}`);
+        const authToken = localStorage.getItem('authToken');
+        try {
+            const response = await fetch(`http://localhost:5678/api/works/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': `Bearer ${authToken}`
+                }
+            });
+            if (response.ok) {
+                console.log(`Successfully deleted ID: ${id}`);
+                element.remove();
+            } else {
+                console.error(`Erreur lors de la suppression de l'appartement, status: ${response.status}`);
+            }
+        } catch (error) {
+            console.error('Erreur lors de la requête de suppression:', error);
+        }
+    }
+
     function filterAppartements(category) {
-        // Sélectionne tous les éléments avec la classe appartement dans l'élément listeFilm
         const allAppartements = document.querySelectorAll('#listeFilm .appartement');
-        // Parcourt tous les appartements et ajuste leur affichage en fonction de la catégorie
         allAppartements.forEach(appartement => {
             if (category === 'all' || appartement.dataset.category === category) {
-                // Affiche l'appartement si la catégorie correspond ou si "Tous" est sélectionné
                 appartement.style.display = 'block';
             } else {
-                // Cache l'appartement si la catégorie ne correspond pas
                 appartement.style.display = 'none';
             }
         });
     }
 
-    // Ajoute un écouteur d'événements pour gérer les clics sur les boutons de filtre
     document.getElementById('filters').addEventListener('click', (event) => {
         if (event.target.classList.contains('filter-btn')) {
-            // Récupère la catégorie à partir de l'attribut data-category du bouton cliqué et la met en minuscules
             const category = event.target.dataset.category.toLowerCase();
-            // Appelle la fonction pour filtrer les appartements en fonction de la catégorie
             filterAppartements(category);
         }
     });
 
-    // Fonction pour supprimer un appartement
-    function deleteAppartement(id) {
-        fetch(`http://localhost:5678/api/works/${id}`, {
-            method: 'DELETE',
-        }).then(response => {
-            if (response.ok) {
-                // Supprime l'élément du DOM
-                document.querySelectorAll(`[data-id="${id}"]`).forEach(element => element.remove());
-            } else {
-                console.error('Erreur lors de la suppression de l\'appartement');
+    document.getElementById('add-photo').addEventListener('change', previewImage);
+
+    function previewImage(event) {
+        const defaultImage = document.getElementById('default-image');
+        const input = event.target;
+        const image = document.getElementById('image-reader');
+
+        if (input.files && input.files[0]) {
+            const reader = new FileReader();
+            reader.onload = function(event) {
+                image.src = event.target.result;
             }
-        }).catch(error => {
-            console.error('Erreur lors de la requête de suppression:', error);
-        });
+            defaultImage.style.display = 'none';
+            image.style.display = 'block';
+            reader.readAsDataURL(input.files[0]);
+        }
     }
 
-    // Appelle la fonction pour récupérer et afficher les appartements au chargement de la page
+    document.getElementById('add-photo-form').addEventListener('submit', async (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+        const authToken = localStorage.getItem('authToken');
+        console.log('Submitting new photo with form data:', formData);
+
+        try {
+            const response = await fetch('http://localhost:5678/api/works', {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${authToken}`
+                },
+                body: formData
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            const newPhoto = await response.json();
+            console.log('New photo added:', newPhoto);
+
+            // Ajoute la nouvelle photo à la galerie principale
+            addPhotoToGallery(newPhoto);
+            // Ajoute la nouvelle photo à la modale
+            addPhotoToModal(newPhoto);
+
+            // Réinitialise le formulaire
+            event.target.reset();
+            // Réinitialiser la prévisualisation de l'image
+            document.getElementById('image-reader').style.display = 'none';
+            document.getElementById('default-image').style.display = 'block';
+
+        } catch (error) {
+            console.error('Erreur lors de l\'ajout de la photo:', error);
+        }
+    });
+
+    function addPhotoToGallery(photo) {
+        console.log('Adding photo to gallery:', photo);
+        const galerie = document.getElementById('listeFilm');
+        const div = document.createElement('figure');
+        div.className = 'appartement';
+        div.dataset.id = photo.id;
+        div.dataset.category = photo.category.name.toLowerCase();
+
+        const title = document.createElement('figcaption');
+        title.textContent = photo.title;
+
+        const img = document.createElement('img');
+        img.src = photo.imageUrl;
+        img.alt = photo.title;
+
+        const deleteIcon = document.createElement('div');
+        deleteIcon.className = 'delete-icon';
+        deleteIcon.innerHTML = '<i class="fa-regular fa-trash-can"></i>';
+
+        deleteIcon.addEventListener('click', (event) => {
+            event.stopPropagation();
+            console.log(`Delete icon clicked for ID: ${photo.id}`);
+            deleteAppartement(photo.id, div);
+        });
+
+        div.appendChild(img);
+        div.appendChild(title);
+        div.appendChild(deleteIcon);
+        galerie.appendChild(div);
+    }
+
+    function addPhotoToModal(photo) {
+        console.log('Adding photo to modal:', photo);
+        const galleryContent = document.querySelector('.gallery-content');
+        const div = document.createElement('figure');
+        div.className = 'appartement-modal';
+        div.dataset.id = photo.id;
+
+        const img = document.createElement('img');
+        img.src = photo.imageUrl;
+        img.alt = photo.title;
+
+        const deleteIcon = document.createElement('div');
+        deleteIcon.className = 'delete-icon';
+        deleteIcon.innerHTML = '<i class="fa-regular fa-trash-can"></i>';
+
+        deleteIcon.addEventListener('click', (event) => {
+            event.stopPropagation();
+            console.log(`Delete icon clicked for ID: ${photo.id}`);
+            deleteAppartement(photo.id, div);
+        });
+
+        div.appendChild(img);
+        div.appendChild(deleteIcon);
+        galleryContent.appendChild(div);
+    }
+
     fetchAppartements();
 
-    // Code pour gérer l'authentification et le mode édition
     const authLink = document.getElementById('auth-link');
     const editModeBar = document.getElementById('edit-mode-bar');
     const filters = document.getElementById('filters');
@@ -162,4 +257,22 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
         filters.style.display = 'flex';
     }
+
+    const modal = document.getElementById('modal');
+    const manageWorksBtn = document.getElementById('manage-works-btn');
+    const closeBtn = document.querySelector('.close-btn');
+
+    manageWorksBtn.addEventListener('click', () => {
+        modal.style.display = 'block';
+    });
+
+    closeBtn.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+    window.addEventListener('click', (event) => {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    });
 });
